2778c7e842d1bf0df3e7a9a0035139593a075c99
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2025-04-02 18:51:09 +0200

    proof-of-concept client

--- client.gd
+++ client.gd
@@ -3,27 +3,49 @@ extends Node
 
 var status: int = 0
 @onready var stream: StreamPeerTCP = StreamPeerTCP.new()
+@onready var timeout_timer: Timer = Timer.new()
 
 const proto = preload("res://proto/data.proto.gd")
 
+var timeout: bool = false
+
 func _ready() -> void:
 	status = stream.get_status()
+	timeout_timer.autostart = false
+	timeout_timer.one_shot = true
+	timeout_timer.wait_time = 5.0
+	add_child(timeout_timer)
+	timeout_timer.timeout.connect(_on_connection_timeout)
 
-func connect_to_host(host: String, port: int) -> void:
+func connect_to_host(host: String, port: int) -> String:
 	print("Connecting to %s:%d" % [host, port])
 	status = stream.STATUS_NONE
 
 	if stream.connect_to_host(host, port) != OK:
 		print("ERROR connecting to host.")
-		return
+		return ""
+		
+	print("INFO connected to ", stream.get_connected_host(), ':', stream.get_connected_port())
+	print("INFO port ", stream.get_local_port())
 	
-	while (stream.get_available_bytes() == 0):
+	timeout_timer.start()
+	while (stream.get_status() == StreamPeerTCP.STATUS_CONNECTING):
+		if timeout:
+			print("ERROR timeout")
+			stream.disconnect_from_host()
+			return ""
+		await get_tree().create_timer(0.1).timeout
 		stream.poll()
+	
+	if stream.get_status() == StreamPeerTCP.STATUS_ERROR:
+		print("ERROR CONNECTING!!!")
+		return "FUCK"
+	
 	var length = stream.get_available_bytes()
 	var bytes = stream.get_data(length)
 	if bytes[0] != OK:
 		print("ERROR on receive")
-		return
+		return ""
 	bytes = bytes[1]
 
 	print("received %d bytes" % [length])
@@ -33,7 +55,9 @@ func connect_to_host(host: String, port: int) -> void:
 	var res = data.from_bytes(bytes)
 	if res != proto.PB_ERR.NO_ERRORS:
 		print("ERROR could not deserialize")
-		return
+		return ""
 	
-	print(data.get_value())
-	print(data.get_timestamp())
+	return "value: " + str(data.get_value()) + ", time: " + str(data.get_timestamp())
+
+func _on_connection_timeout() -> void:
+	timeout = true
--- main.gd
+++ main.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-const HOST: String = "127.0.0.1"
-const PORT: int = 8080
+const HOST: String = "51.20.64.237"
+const PORT: int = 7000
 
 @onready var client: Client = Client.new()
 
+@onready var button: Button = %Button
+@onready var label: Label = %Label
+
 func _ready() -> void:
-    add_child(client)
-    client.connect_to_host(HOST, PORT)
+	add_child(client)
+	button.pressed.connect(_on_button_pressed)
+
+
+func _on_button_pressed() -> void:
+	var res = await client.connect_to_host(HOST, PORT)
+	print(res)
+	label.text = res
